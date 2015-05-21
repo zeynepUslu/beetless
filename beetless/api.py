618512bcc4c83cf4ba94b5a2ea07700ec2f40496
H. Turgut Uyar <uyar@itu.edu.tr>
2015-05-21 18:21:31 +0300

    started implementing the ReST API

---
+++ beetless/api.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2015 H. Turgut Uyar <uyar@itu.edu.tr>
+#
+# This file is part of Beetless.
+#
+# Kurander is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Beetless is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Beetless.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Module containing the ReST API for Beetless."""
+
+from beetless import bottle
+
+
+@bottle.route('/api/v1/artists')
+def get_artists():
+    """Get the artists in the collection.
+
+    :return: All artists in the collection.
+    :rtype: dict[str, list]
+    """
+    return {'artists': []}
+
+
+def start(host='localhost', port=8080):
+    """Start the API service.
+
+    :param str host: Server address to bind to.
+    :param int port: Server port to bind to.
+    """
+    bottle.run(host=host, port=port)
+
+
+if __name__ == '__main__':
+    start()
